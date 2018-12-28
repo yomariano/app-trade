6054c4d7f0f1500a5cf28358d1c3b78e9b5320f7
DR\mariano.rodriguez <mariano.rodriguez@citywonders.com>
2018-12-28 15:09:25 +0000

    push notification test working example

--- App.js
+++ App.js
@@ -1,42 +1,102 @@
-import {Platform} from 'react-native';
-import {createAppContainer, createMaterialTopTabNavigator } from 'react-navigation';
-import Balance from './src/screens/Balance';
-import Trades from './src/screens/Trades';
-import CreateOrder from './src/screens/CreateOrder';
+import React, { Component } from "react";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import NotifService from './src/screens/NotifService';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+  Alert
+} from "react-native";
+import keys from "./src/server/config";
 
-const NavStack = createMaterialTopTabNavigator({
-  Balance: { 
-      screen: Balance,
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      senderId: keys.SENDER_ID
+    };
+
+    this.notif = new NotifService(this.onRegister.bind(this), this.onNotif.bind(this));
+  }
+
+  
+  onRegister(token) {
+    Alert.alert("Registered !", JSON.stringify(token));
+    console.log(token);
+    this.setState({ registerToken: token.token, gcmRegistered: true });
+  }
+
+  onNotif(notif) {
+    console.log(notif);
+    Alert.alert(notif.title, notif.message);
+  }
+
+  handlePerm(perms) {
+    Alert.alert("Permissions", JSON.stringify(perms));
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Example app react-native-push-notification</Text>
+        <View style={styles.spacer}></View>
+        <TextInput style={styles.textField} value={this.state.registerToken} placeholder="Register token" />
+        <View style={styles.spacer}></View>
+
+        <TouchableOpacity style={styles.button} onPress={() => { this.notif.localNotif() }}><Text>Local Notification (now)</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => { this.notif.scheduleNotif() }}><Text>Schedule Notification in 30s</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => { this.notif.cancelNotif() }}><Text>Cancel last notification (if any)</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => { this.notif.cancelAll() }}><Text>Cancel all notifications</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => { this.notif.checkPermission(this.handlePerm.bind(this)) }}><Text>Check Permission</Text></TouchableOpacity>
+
+        <View style={styles.spacer}></View>
+        <TextInput style={styles.textField} value={this.state.senderId} onChangeText={(e) => {this.setState({ senderId: e })}} placeholder="GCM ID" />
+        <TouchableOpacity style={styles.button} onPress={() => { this.notif.configure(this.onRegister.bind(this), this.onNotif.bind(this), this.state.senderId) }}><Text>Configure Sender ID</Text></TouchableOpacity>
+        {this.state.gcmRegistered && <Text>GCM Configured !</Text>}
+
+        <View style={styles.spacer}></View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  Trades: { 
-    screen: Trades,
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
-  CreateOrder: { 
-    screen: CreateOrder,
+  button: {
+    borderWidth: 1,
+    borderColor: "#000000",
+    margin: 5,
+    padding: 5,
+    width: "70%",
+    backgroundColor: "#DDDDDD",
+    borderRadius: 5,
+  },
+  textField: {
+    borderWidth: 1,
+    borderColor: "#AAAAAA",
+    margin: 5,
+    padding: 5,
+    width: "70%"
+  },
+  spacer: {
+    height: 10,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 20,
+    textAlign: "center",
   }
 });
-
-const App = createAppContainer(NavStack);
-
-export default App;
-
-// export default class App extends Component {
-  
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.welcome}>Welcome to React Native!</Text>
-//         <Text style={styles.instructions}>To get started, edit App.js</Text>
-//         <Text style={styles.instructions}>{instructions}</Text>
-//       </View>
-//     );
-//   }
-// }
 
